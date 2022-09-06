5b5301390cae1ea0e9a216f813bae5fa32934abe
홍길동 <yorsild@naver.com>
2022-09-06 22:56:59 +0900

    테스트 UI 코드 작성

--- app/src/main/java/org/devlion/catchCall/MainActivity.java
+++ app/src/main/java/org/devlion/catchCall/MainActivity.java
@@ -1,5 +1,6 @@
 package org.devlion.catchCall;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -8,13 +9,53 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.devlion.util.cmn.Receiver;
+import org.devlion.util.cmn.HttpHelper;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "CATCH_CALL";
+    TextView txv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermission();
+
+        HttpHelper httpHelper = new HttpHelper();
+        httpHelper.setReceiver(new Receiver() {
+            @Override
+            public void onResponse(int resCode, JSONObject res) {
+                Log.d(TAG, "resCode: " + resCode + ", res: " + res);
+                txv.setText("JSON: " + res);
+            }
+        });
+
+        txv = findViewById(R.id.txv);
+        txv.setText("Hello");
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Click!", Toast.LENGTH_SHORT).show();
+
+                Map<String, String> req = new HashMap<String, String>();
+                req.put("option", "01012341234");
+                httpHelper.doGet(req);
+            }
+        });
     }
 
     public void checkPermission(){
