a638a10735bb7fc3d4d9271dc40e3229a7f9f6b1
홍길동 <yorsild@naver.com>
2022-09-08 01:11:20 +0900

    (오류 수정) 전화벨 감지

--- app/src/main/java/org/devlion/catchCall/CatchCallReceiver.java
+++ app/src/main/java/org/devlion/catchCall/CatchCallReceiver.java
@@ -11,37 +11,33 @@ import android.widget.Toast;
 public class CatchCallReceiver extends BroadcastReceiver {
 
     final String TAG = "CATCH_CALL";
+    private static String mLastState;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive()");
 
         String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+        String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
 
-        if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
+        // 전화밸이 울리면 Receiver가 2번씩 수신되므로 incomingNumber == null 일 경우 종료
+        if(incomingNumber == null){
+            Log.d(TAG, "incomingNumber is null");
+            return;
+        }
 
-            String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+        if(TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
 
-            if(incomingNumber != null){
-                Log.d(TAG, "incomingNumber:" + incomingNumber);
-                final String phoneNumber = PhoneNumberUtils.formatNumber(incomingNumber);
-                Log.d(TAG, "phoneNumber:" + phoneNumber);
-                Toast.makeText(context,"Call: " + phoneNumber, Toast.LENGTH_SHORT).show();
-            }
-
-//            final String phoneNumber = PhoneNumberUtils.formatNumber(incomingNumber);
-//
-//            Intent serviceIntent = new Intent(context, CallingService.class);
-//            serviceIntent.putExtra(CallingService.EXTRA_CALL_NUMBER, phone_number);
-//            context.startService(serviceIntent);
-//            Log.d(TAG, "Call:"+incomingNumber);
+            final String phoneNumber = PhoneNumberUtils.formatNumber(incomingNumber);
+            Log.d(TAG, "phoneNumber:" + phoneNumber);
+            Toast.makeText(context, "Call: " + phoneNumber, Toast.LENGTH_SHORT).show();
 
         }else if(TelephonyManager.EXTRA_STATE_OFFHOOK.equals(state)){
-            Log.d(TAG, "EXTRA_STATE_OFFHOOK 전화수신");
+            Log.d("CatchCall", "EXTRA_STATE_OFFHOOK 전화수신");
 
 
         }else if(TelephonyManager.EXTRA_STATE_IDLE.equals(state)){
-            Log.d(TAG, "EXTRA_STATE_IDLE 전화거절/통화밸 종료");
+            Log.d("CatchCall", "EXTRA_STATE_IDLE 전화거절/통화밸 종료");
 
         }
     }
